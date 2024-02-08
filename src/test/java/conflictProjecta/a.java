7d02eabe760c66d0496d20a1dec3494a5e929074
user <user@DESKTOP-EULJV3S>
2024-02-08 14:26:52 +0530

    s

--- src/test/java/conflictProjecta/a.java
+++ src/test/java/conflictProjecta/a.java
@@ -5,8 +5,8 @@ public class a {
 	public static void main(String[] args) {
 		System.out.println("rocky");
 		System.out.println("rokeshbro");
-		System.out.println("Hi this is rakesh");
-		System.out.println("conflict 2");
+		System.out.println("nkjkabkjb mn d  asc jsajchv avc");
+		System.out.println("mklasanckascbcascvhavc");
 	}
 
 }
